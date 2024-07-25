3662086e60550a2a148c8d41da4c64f57629d372
Parimal Matte <pmprofessional26@gmail.com>
2024-07-25 13:08:38 +0530

    just commit

---
+++ e_wholesaler/src/main/java/com/parimal/e_wholesaler/e_wholesaler/EWholesalerApplication.java
@@ -0,0 +1,13 @@
+package com.parimal.e_wholesaler.e_wholesaler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EWholesalerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EWholesalerApplication.class, args);
+	}
+
+}
---
+++ e_wholesaler/src/test/java/com/parimal/e_wholesaler/e_wholesaler/EWholesalerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parimal.e_wholesaler.e_wholesaler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EWholesalerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
