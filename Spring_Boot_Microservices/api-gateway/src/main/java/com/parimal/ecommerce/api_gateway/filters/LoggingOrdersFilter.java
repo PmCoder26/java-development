1d0f719aa263c6d0e77e64124dff254b98edd35f
Parimal Matte <pmprofessional26@gmail.com>
2024-11-11 21:08:22 +0530

    API Gateway: GlobalLoggingFilter and LoggingOrdersFilter commit

---
+++ Spring_Boot_Microservices/api-gateway/src/main/java/com/parimal/ecommerce/api_gateway/filters/LoggingOrdersFilter.java
@@ -0,0 +1,40 @@
+package com.parimal.ecommerce.api_gateway.filters;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import static reactor.core.publisher.Mono.fromRunnable;
+
+@Component
+@Slf4j
+//  Note - don't forget to register this filter in the application.yml file. As the filter is not registered for a specific route,
+//         so it won't work.
+public class LoggingOrdersFilter extends AbstractGatewayFilterFactory<LoggingOrdersFilter.ConfigClass> {
+
+    public LoggingOrdersFilter(){
+        super(ConfigClass.class);
+    }
+
+    @Override
+    public GatewayFilter apply(ConfigClass config) {        // useful when you want to process more variables/arguments.
+        return (exchange, chain) -> {
+            log.info("Order Filter Pre: {}", exchange.getRequest().getURI());
+            return chain
+                    .filter(exchange)
+                    .then(fromRunnable(() -> {
+                        log.info("Order Filter Logging Post: {}", exchange.getResponse().getStatusCode());
+                    }));
+        };
+    }
+
+
+    public static class ConfigClass {
+
+    }
+
+
+}
