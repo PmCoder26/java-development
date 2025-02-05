78d1ce1e00b143fe469bdf8f84c93b6fddafb686
Parimal Matte <123257338+PmCoder26@users.noreply.github.com>
2025-02-05 07:20:29 +0530

    spring-boot-kubernetes: initial commit

---
+++ SpringBoot_Kubernetes/order_service/src/test/java/com/parimal/e_wholesaler/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parimal.e_wholesaler.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
